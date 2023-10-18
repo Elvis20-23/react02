6cd95c6fb906cc8153b43e82ff5289f16b4378f0
Elvis20-23 <kiubyes.15@gmail.com>
2023-10-18 18:24:14 -0500

    v5. Diseño de App y AppForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import AppForm from './componentes/AppForm';
 import logo from './logo.svg';
 //import './App.css';
 import C01componente from './pagina/C01componente';
 
 function App() {
   return (
-    <div>
-      <C01componente/>
-      hola mundo desde react
-      <i class="large material-icons">insert_chart</i>
-
+    <div style={{width:"350px", background:"greenyellow",padding:"10px"}}>
+      <AppForm  />
+      <p>No. 1 Juan Manuel x A</p>
+      <p>No. 1 Rosa Maria x A</p>
+      
     </div>
   );
 }
---
+++ src/componentes/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuvCF3BhGK7sqjZXRt3rKqCJdO-KE5DJ8",
+  authDomain: "appreact-b2549.firebaseapp.com",
+  projectId: "appreact-b2549",
+  storageBucket: "appreact-b2549.appspot.com",
+  messagingSenderId: "1079878316312",
+  appId: "1:1079878316312:web:372c172ae1f0e04526ee6d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
